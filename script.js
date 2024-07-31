f4bf92616d38f32ff3e662eb3b08f2e1404eff75
Thomas Gurr <tpgurr21@gmail.com>
2024-07-30 20:51:12 -0400

    intro to OOP

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
 'use strict';
 
+const Person = function(firstName, birthYear) {
+    // Instance properties
+    //  console.log(this);
+    this.firstName = firstName;
+    this.birthYear = birthYear;
 
+    // Never do this (create a method in a constructor function)
+    // this.calcAge = function() {
+    //     console.log(2037 - this.birthYear);
+    // }
+}
+
+const thomas = new Person('Thomas', 1994);
+console.log(thomas)
+
+// 1. New {} is created
+// 2. function is called, this = {}
+// 3. {} linked to prototype
+// 4. function automatically returns {}
+
+const jacob = new Person('Jacob', 2017);
+const jack = new Person('Jack', 1975);
+console.log(jacob, jack);
+
+
+console.log(thomas instanceof Person); // True or False
