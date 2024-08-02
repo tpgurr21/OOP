43c17998977cd9ef89dd908994792ed7d4d41dc6
Thomas Gurr <tpgurr21@gmail.com>
2024-08-02 18:01:13 -0400

    Object.create

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-
+/*
 
 const Person = function(firstName, birthYear) {
     // Instance properties
@@ -237,4 +237,27 @@ PersonCl.hey();
 // account.latest = 50
 // console.log(account.movements)
 
+*/
 
+const PersonProto = {
+    calcAge() {
+        console.log(2037 - this.birthYear)
+    },
+
+    init(firstName, birthYear) {
+        this.firstName = firstName;
+        this. birthYear = birthYear;
+    }
+};
+
+const steven = Object.create(PersonProto);
+console.log(steven)
+steven.name = 'Steven';
+steven.birthYear = 2002;
+steven.calcAge();
+
+console.log(steven.__proto__ === PersonProto);
+
+const sarah = Object.create(PersonProto);
+sarah.init('Sarah', 1979);
+sarah.calcAge();
