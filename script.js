338bda8929af5da3e7b4c45d6b54124c3d0558ce
Thomas Gurr <tpgurr21@gmail.com>
2024-08-03 22:18:39 -0400

    Inheritance with Object.create

--- script.js
+++ script.js
@@ -388,7 +388,7 @@ toyota.brake();
 toyota.accelerate();
 toyota.accelerate();
 
-*/
+
 
 class PersonCl {
     constructor(fullName, birthYear) {
@@ -442,3 +442,35 @@ constructor(fullName, birthYear, course) {
 const martha = new StudentCl('Martha Jones', 2012, 'Computer Science');
 martha.introduce();
 martha.calcAge();
+
+*/
+
+////////////////////////////
+// Inheritance Between "Classes": Object.create
+
+const PersonProto = {
+    calcAge() {
+        console.log(2037 - this.birthYear)
+    },
+
+    init(firstName, birthYear) {
+        this.firstName = firstName;
+        this. birthYear = birthYear;
+    }
+};
+
+const steven = Object.create(PersonProto);
+
+const StudentProto = Object.create(PersonProto);
+StudentProto.init = function(firstName, birthYear, course) {
+    PersonProto.init.call(this, firstName, birthYear);
+    this.course = course;
+}
+
+StudentProto.introduce = function () {
+    console.log(`My name is ${this.firstName} and I study ${this.course}`);
+}
+
+const jay = Object.create(StudentProto);
+jay.init('Jay', 2010, 'Computer Science');
+jay.calcAge();
