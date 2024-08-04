19665a4086f53a1f5c5370023ca31f8014a8ee42
Thomas Gurr <tpgurr21@gmail.com>
2024-08-03 22:29:42 -0400

    more work with classes

--- script.js
+++ script.js
@@ -443,7 +443,7 @@ const martha = new StudentCl('Martha Jones', 2012, 'Computer Science');
 martha.introduce();
 martha.calcAge();
 
-*/
+
 
 ////////////////////////////
 // Inheritance Between "Classes": Object.create
@@ -474,3 +474,49 @@ StudentProto.introduce = function () {
 const jay = Object.create(StudentProto);
 jay.init('Jay', 2010, 'Computer Science');
 jay.calcAge();
+
+*/
+
+class Account {
+    constructor(owner, currency, pin) {
+        this.owner = owner;
+        this.currency = currency;
+        this.pin = pin;
+        this.movements = [];
+        this.locale = navigator.language;
+
+        console.log(`Thanks for opening an account, ${owner}`);
+    }
+
+    // Public interface
+    deposit(val) {
+        this.movements.push(val)
+    }
+
+    withdraw(val) {
+        this.deposit(-val)
+    }
+
+    approveLoan(val) {
+        return true;
+    }
+
+    requestLoan(val) {
+        if(this.approveLoan(val)) {
+            this.deposit(val);
+            console.log(`Your loan of ${val} was approved`);
+        }
+    }
+}
+
+const acc1 = new Account('Jonas', 'EUR', 1111, []);
+
+// acc1.movements.push(250);
+// acc1.movements.push(-140);
+acc1.deposit(250);
+acc1. withdraw(140);
+acc1.requestLoan(1000);
+acc1.approveLoan(1000);
+
+console.log(acc1);
+console.log(acc1.pin);
